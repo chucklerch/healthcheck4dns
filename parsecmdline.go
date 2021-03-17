8ca6e48b469481ead5cee4314b31dcf6ebf5f8f8
“Chuck <chucklerch@gmail.com>
2021-03-16 20:54:49 -0400

    add command line parsing

---
+++ parsecmdline.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+)
+
+func parseCmdline() {
+	// Setup command line arguments
+	helpArg := flag.Bool("help", false, "Display help.")
+	versionArg := flag.Bool("version", false, "Display version.")
+	configArg := flag.String("config", "config.yaml", "Use configuration file.")
+	debugArg := flag.Bool("debug", false, "Enable debugging outout")
+
+	flag.Parse()
+	if *helpArg {
+		flag.PrintDefaults()
+		os.Exit(0)
+	}
+	if *versionArg {
+		fmt.Println("Version: " + Version)
+		os.Exit(0)
+	}
+	if *debugArg {
+		debug = true
+	}
+	// if the config file is set, use it.
+	if *configArg != "" {
+		configFile = *configArg
+		log.Printf("Using config file %s\n", configFile)
+
+	}
+}
